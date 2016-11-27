b35ef439520de386fd022c0ba954821392c8f695
Titus_R <titusrotich2010@gmail.com>
2016-11-27 12:10:31 +0300

    personal blog

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class post(models.Model):
     title = models.CharField(max_length=140)
     body = models.TextField()
-    date =  models.DateTimeField()
+    date = models.DateTimeField()
 
     def __str__(self):
         return self.title
--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from django.views.generic import ListView, DetailView
-from blog.models import Post
-
+from blog.models import post
+ 
 urlpatterns = [ url(r'^$',ListView.as_view(queryset=post.objects.all().order_by("-date")[:25],template_name="blog/blog.html"))]
 
 # Create your views here.
